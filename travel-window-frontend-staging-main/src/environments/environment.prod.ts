6c5344629ace921ab603b10dac4e661e234e837a
Admin <harmansinghdhaliwal032@gmail.com>
2026-04-30 14:03:03 +0530

    Update default API URLs and environment fallbacks

--- travel-window-frontend-staging-main/src/environments/environment.prod.ts
+++ travel-window-frontend-staging-main/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://travel-window-backend.vercel.app/api', // Will be replaced by build script
+  apiUrl: 'https://travel-windo-ca-s5b9.vercel.app/api', // Will be replaced by build script
   recaptchaSiteKey: '' // Will be replaced by build script
 };
